c206d4518ab0af4a58be392e9e641d631adc8d0f
Salman Ansari <salman137138@gmail.com>
2025-07-15 13:48:05 +0530

    env.production added

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -1,4 +1,6 @@
 const BASE_URL = import.meta.env.VITE_BASE_API_URL;
+
+
 export const USER_API_END_POINT=`${BASE_URL}/api/v1/user`;
 export const JOB_API_END_POINT=`${BASE_URL}/api/v1/job`;
 export const APPLICATION_API_END_POINT=`${BASE_URL}/api/v1/application`;
